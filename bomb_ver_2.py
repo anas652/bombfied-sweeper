eadfc3ac6ff6700a5c9fdd841d2a5f097c0d2408
Anas Khmammash <anasfortnite@yahoo.com>
2020-03-17 12:49:41 +0300

    bombs

---
+++ bomb_ver_2.py
@@ -0,0 +1,77 @@
+import tkinter
+import random
+
+game_over = False
+score = 0 
+squarestoclear = 0
+
+def play_bombdodger():
+	createbombfield(bombfield)
+	window=tkinter.Tk()
+	layout_window(window)
+	window.mainloop()
+
+
+bombfield = []
+def createbombfield(bombfield):
+	global squarestoclear
+	for row in range(0,10):
+		rowlist = []
+		for column in range(0,10):
+			if random.randint(1,100) < 20:
+				rowlist.append(1)
+			else:
+				rowlist.append(0)
+				squarestoclear = squarestoclear + 1
+		bombfield.append(rowlist)
+	# printfield(bombfield)
+
+
+def printfield(bombfield):
+	for rowlist in bombfield:
+		print(rowlist)
+
+
+
+	
+
+def layout_window(window):
+	for rowNumber, rowlist in enumerate(bombfield):
+		for columnNumber, columnEntry in enumerate(rowlist):
+			if random.randint(1,90) < 30:
+				square = tkinter.Label(window, text = "    ", bg = "darkgreen")
+			elif random.randint(1,100) > 60:
+				square = tkinter.Label(window, text = "    ", bg = "sea green")
+			else:
+				square = tkinter.Label(window, text = "    ", bg = "darkseagreen")
+			square.grid(row = rowNumber, column = columnNumber)
+			square.bind("<Button-1>", on_click) 
+
+
+def on_click(event):
+	global score
+	global game_over
+	global squarestoclear
+	square = event.widget
+
+	row = int(square.grid_info()["row"])
+
+	column = int(square.grid_info()["column"])
+
+
+
+
+play_bombdodger()
+
+
+
+
+
+
+
+
+
+
+
+
+
